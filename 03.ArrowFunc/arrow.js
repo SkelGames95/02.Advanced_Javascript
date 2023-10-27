a86651fec1604006ce18bc29a66743470321b1b5
Marco.Polenta <polentamarco.dev@gmail.com>
2023-10-27 20:38:43 +0200

    exercise done

---
+++ 03.ArrowFunc/arrow.js
@@ -0,0 +1,19 @@
+const sum = (a, b) =>  a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const log = (value) => console.log(value);
+
+const calculate = (callback) => callback();
+
+const result = calculate(() =>
+    divide(subtract(multiply(sum(2, 4), sum(5, 2)),2),5));
+
+log(result);
+
+let x = (((2 + 4) * (5 + 2) - 2) / 5); // ho fatto una prova per vedere se si potesse fare nel pratico senza dover ricorrere a tutta questa procedura
+console.log(x);
