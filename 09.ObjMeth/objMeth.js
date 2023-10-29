dd6d7ad2d9f84c1def527c198c5f590a97fcc9e4
Marco.Polenta <polentamarco.dev@gmail.com>
2023-10-29 23:29:54 +0100

    exercise done

---
+++ 09.ObjMeth/objMeth.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
+};
+
+const keys = Object.keys(person);
+
+keys.forEach((key) => {
+    console.log(`${key}: ${person[key]}`);
+});
