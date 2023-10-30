685a57acda9118a88ffffdf925e9f451df0fcb2c
Marco.Polenta <polentamarco.dev@gmail.com>
2023-10-30 01:50:27 +0100

    exercise done

---
+++ 12.ArrMtdsAdv2/nick.js
@@ -0,0 +1,23 @@
+function nicknameMap(persons) {
+    return persons.map((el) => el.name + "-" + el.age);
+}
+
+const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log("All Persons:");
+console.log(persons);
+console.log("----------------------------------------------------------------");
+console.log("Nicknames:");
+console.log(nicknames);
