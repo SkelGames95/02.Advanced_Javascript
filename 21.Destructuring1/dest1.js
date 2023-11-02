3d9dbc94495a3b525f88abcb00d24bdf77872763
Marco.Polenta <polentamarco.dev@gmail.com>
2023-11-02 01:53:23 +0100

    exercise done

---
+++ 21.Destructuring1/dest1.js
@@ -0,0 +1,33 @@
+let num1 = 10;
+let num2 = 20;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+// Variable swapping
+let temp = num2;
+num2 = num1;
+num1 = temp;
+
+console.log('After swap: ', num1, num2); // After swap: 20 10
+console.log("----------------------------------------------")
+
+let obj = {a: 10, b:20};
+
+console.log('Before swap:');
+console.log(obj);
+
+let {b, a} = obj;
+
+console.log('After swap:');
+console.log(b + ", " + a);
+
+console.log("----------------------------------------------")
+
+let arr = [10, 20];
+
+console.log('Before swap:');
+console.log(arr);
+
+let [d, c] = arr;
+
+console.log('After swap:');
+console.log(c + ", " + d);
