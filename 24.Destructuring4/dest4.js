37c1d8890add97dbf48c797b4bedb0bc05c59f08
Marco.Polenta <polentamarco.dev@gmail.com>
2023-11-03 17:04:38 +0100

    exercise done

--- 24.Destructuring4/dest4.js
+++ 24.Destructuring4/dest4.js
@@ -5,17 +5,17 @@ const person = {
     age: 25
 };
 
-// const { id, firstName, lastName, age, name = "N/A", surname = "N/A", old = "N/A" } = person;
-// console.log(id, firstName, lastName, age, name, surname, old);
-
-// ||
-
-const person2 = {
-    id: 1,
-    name: 'Mario',
-    surname: 'Rossi',
-    old: 25
-};
-
-const { id, name, surname, old } = person2;
+const { id, firstName: name, lastName: surname, age: old } = person;
 console.log(id, name, surname, old);
+
+
+
+// const person2 = {
+//     id: 1,
+//     name: 'Mario',
+//     surname: 'Rossi',
+//     old: 25
+// };
+
+// const { id, name, surname, old } = person2;
+// console.log(id, name, surname, old);
