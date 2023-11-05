84f5d0869c0b0bbd9689545299d9e188594c5450
Marco.Polenta <polentamarco.dev@gmail.com>
2023-11-05 06:07:21 +0100

    exercise completed

---
+++ 25.RestSpread1/RS1.js
@@ -0,0 +1,10 @@
+// let numberStore = [0, 1, 2];
+// let newNumber = 3;
+// numberStore.push(newNumber);
+// console.log(numberStore);
+
+let  newNumber = [3];
+let numberStore = [1, 2, ...newNumber];
+
+console.log(numberStore);
+
