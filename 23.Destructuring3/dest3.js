c9785a8bf21c6731660e74815ecc1d4e1e809f29
Marco.Polenta <polentamarco.dev@gmail.com>
2023-11-08 22:37:58 +0100

    exercise updated with correction

--- 23.Destructuring3/dest3.js
+++ 23.Destructuring3/dest3.js
@@ -2,16 +2,8 @@
 //     return person.age >= 18;
 // }
 
-// function isAdult({age}) {
-//     if(age >= 18){
-//         return person;
-//     } else {
-//         let person = "Your age is invalid!";
-//         return person;
-//     }
-// }
 
-// La prossima volta facciamo le cose semplici:
+
 function isAdult({age}) {
     return age >= 18;
 }
@@ -25,10 +17,31 @@ const person = {
 
 console.log(isAdult(person));
 
+// inizialmente risolto con questo metodo:
+
+// function isAdult({age}) {
+//     if(age >= 18){
+//         return person;
+//     } else {
+//         let person = "Your age is invalid!";
+//         return person;
+//     }
+// }
+
+// non hai capito bene l'ambito in cui si usa la funzione, il nome stesso della funzione indica
+// che quella funzione farà semplicemente un controllo sull' età e ritorna un boolean. Questo perché la funzione
+// isAdult() la possiamo usare in un contesto dove ci serve più volte fare un controllo del genere e abbiamo una
+// funzione dedicata allo scopo, poi una volta preso il valore che ritorna la funzione isAdult() possiamo mettere
+// tutti i messaggi che vogliamo ma in un'altra funzione. Per come l'hai fatta tu la funzione mi sta ritornando
+// una variabile che sta fuori dalla funzione e non ha molto senso e per di più non è una buona pratica, quella
+// variabile fuori dalla funzione serve per passarla alla funzione come parametro proprio per fare un controllo,
+// ecco il motivo dei tre punti.
 
 // Voglio aggiungere anche il seguente messaggio/controllo al risultato:
-// (appunto per live o spiegazione Stefano)
-// 
+
 //  if (person == Object){
 //     console.log("Registration confirmed")
 //  }
+
+// potrebbe essere un controllo da fare ma non dentro la funzione isAdult() e magari potresti farlo con il typeof.
+
