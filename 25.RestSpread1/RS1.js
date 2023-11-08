ab6fe3d48b87ed3c25b501c7ce71f7a1751c4940
Marco.Polenta <polentamarco.dev@gmail.com>
2023-11-09 00:36:10 +0100

    exercise updated with correction

--- 25.RestSpread1/RS1.js
+++ 25.RestSpread1/RS1.js
@@ -1,10 +1,31 @@
+// Partendo da:
+
 // let numberStore = [0, 1, 2];
 // let newNumber = 3;
 // numberStore.push(newNumber);
 // console.log(numberStore);
 
-let  newNumber = [3];
-let numberStore = [1, 2, ...newNumber];
+
+
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+
+numberStore = [...numberStore, newNumber];
 
 console.log(numberStore);
 
+// Inizialmente risolto così:
+
+// let  newNumber = [3];
+// let numberStore = [1, 2, ...newNumber];
+
+// console.log(numberStore);
+
+
+
+// Visto il codice di partenza sarebbe stato più giusto fare così: 
+
+// numberStore = [...numberStore, newNumber];
+
+// perché è il valore della variabile newNumber che vogliamo aggiungere 
+// dentro numberStore, quest'ultimo è già un array e quindi usiamo lo spread operator.
