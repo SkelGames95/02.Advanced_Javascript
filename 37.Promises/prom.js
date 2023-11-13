7cadc81390c9a863262a97ded7d4602979ee0168
Marco.Polenta <polentamarco.dev@gmail.com>
2023-11-13 16:41:02 +0100

    answers added

--- 37.Promises/prom.js
+++ 37.Promises/prom.js
@@ -39,3 +39,16 @@ prom
 // Ho voluto fare una prova che mi permettesse di avere a che fare con risultati aleatori
 // senza poter ricorrere al simulatore della console web, ho optato per un altro metodo
 // che mi sembrava relativamente efficace per identificare il caso "rejected". Che ne pensi?
+
+
+// L'esercizio va bene. Per quanto riguarda i casi di fullfilled o reject te ne rendi conto in base
+// alla Promise se entra nel resolve o nel reject. Per il resolve avrai il risultato nel then, mentre
+// per reject invece avrai il risultato nel catch perché di solito reject ritorna un errore, in questo
+// esercizio stiamo ritornando sempre il number quindi non si vede molto. Ti passo questo link: Promise
+// controlla la parte 'Comprendere gli stati delle Promise' per vedere come ispezionare lo stato della
+// Promise con gli strumenti di debug.
+
+// Anche la prova che hai fatto va bene, però per vedere meglio il reject e quindi la promise che entra
+// nel catch, puoi usare questo nel reject: reject(new Error(Rejected: ${op})); così ritorni un'errore
+// custom e il catch stampa l'errore, perché come avevi fatto prima avevi un console.log che stampava
+// undefined ed era quello del catch.
